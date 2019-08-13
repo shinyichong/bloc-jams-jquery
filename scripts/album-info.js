2887d85255f0c6ee42940c22b864f99c9ff102f4
shinyi <shinyic.design@gmail.com>
2019-08-13 14:53:31 -0400

    album info

---
+++ scripts/album-info.js
@@ -0,0 +1,6 @@
+ {
+    $('#album-title').text(album.title);
+    $('img#album-cover-art').attr('src', album.albumArtUrl);
+    $('#album-artist').text(album.artist);
+    $('#album-release-info').text(album.releaseInfo);
+ }
